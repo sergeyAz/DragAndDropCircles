24a17fe8687caaddd54f151a778d6f528f4cb070
sergey <sergeypaltsev@mail.ru>
2017-02-11 04:08:34 -0800

    Radius add

--- app/src/main/java/ee/sergeypaltsev/draganddropcircle/DrawScene.java
+++ app/src/main/java/ee/sergeypaltsev/draganddropcircle/DrawScene.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -17,24 +18,61 @@ public class DrawScene extends View {
 
     float currentX = 100;
     float currentY = 100;
+    float radius = 70;
 
     boolean circleMovable = false;
 
-    public DrawScene(Context context){
+    public DrawScene(Context context) {
         super(context);
         canvasPaint = new Paint();
         canvasPaint.setStyle(Paint.Style.FILL);
         canvasPaint.setColor(Color.YELLOW);
         circlePaint = new Paint();
     }
+
     @Override
     protected void onDraw(Canvas sceneCanvas) {
         super.onDraw(sceneCanvas);
         sceneCanvas.drawPaint(canvasPaint);
         drawCircle(sceneCanvas);
     }
-    private void drawCircle(Canvas sceneCanvas){
+
+    private void drawCircle(Canvas sceneCanvas) {
         circlePaint.setColor(Color.BLUE);
-        sceneCanvas.drawCircle(currentX, currentY, 100, circlePaint);
+        sceneCanvas.drawCircle(currentX, currentY, radius, circlePaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final int action = event.getAction();
+
+        switch (action & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                float a = event.getX(0) - currentX;
+                float b = event.getY(0) - currentY;
+                if (Math.hypot(a,b)<=radius){
+                circleMovable = true;
+            }
+            break;
+            }
+            case MotionEvent.ACTION_UP: {
+                circleMovable = false;
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+
+                if (circleMovable) {
+                    currentX = event.getX(0);
+                    currentY = event.getY(0);
+                }
+
+            }
+
+            break;
+        }
+        this.postInvalidate();
+        return true;
     }
 }
+
+
